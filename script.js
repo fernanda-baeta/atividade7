cfaa91fd97304be0b19353b640596ceafa55f3e3
fernanda-baeta <80594348+fernanda-baeta@users.noreply.github.com>
2021-05-15 00:43:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+nome= prompt ("Digite seu nome");
+anoNascimento= parseInt(prompt ("Digite o ano em que você nasceu"));
+const idade = 2021 - anoNascimento;
+
+alert (`Olá, seu nome é ${nome}, e você tem ${idade} anos`)
